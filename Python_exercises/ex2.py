f7ee7d1a06338fcd5dd0b202a4ec7aab6b6d9b0e
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-05-21 14:55:29 -0500

    Moving up to exercise 5 in Zed's book.

---
+++ Python_exercises/ex2.py
@@ -0,0 +1,10 @@
+#  This is a comment so I can read the program later.
+#  Anything after this is going to be ignored by Python.
+
+# And any code after the octothorpe is ignored.
+print("I could have code like this.")
+
+# You can also use a comment to disable or comment out code.
+#  print("This won't run.")
+
+print('This will run.')
