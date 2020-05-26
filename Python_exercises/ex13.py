6899608ff0864a1a47259f70756c07ce01ff6be6
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-05-25 22:48:45 -0500

    Figured out how to combine module imports, arguments, and input().

---
+++ Python_exercises/ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+query = input("What's your name?")
+
+print("The script is called: ", script)
+print("The first variable is called: ", first)
+print("The second variable is: ", second)
+print("The third variable is: ", third)
+
+print(f"{query}: Here are your favorite characters - {first}, {second}, {third}.")
