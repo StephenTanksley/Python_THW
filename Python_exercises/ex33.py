2f094c98f2465b68a871afc0389e53b40ca8447d
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-11-02 14:35:59 -0600

    Extended exercise 33 to be more flexible.

--- Python_exercises/ex33.py
+++ Python_exercises/ex33.py
@@ -1,15 +1,32 @@
 i = 0
 numbers = []
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+# while i < 6:
+#     print(f"At the top i is {i}")
+#     numbers.append(i)
 
-    i = i + 1
+#     i = i + 1
 
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
+#     print("Numbers now: ", numbers)
+#     print(f"At the bottom i is {i}")
 
-print("The numbers: ")
-for num in numbers:
-    print(num)
+# print("The numbers: ")
+# for num in numbers:
+#     print(num)
+
+
+def while_loop(num, increment):
+    i = 0
+    numbers = []
+    while i < 6:
+        print(f"At the top i is {i}")
+        numbers.append(i)
+
+        i = i + increment
+
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
+
+    print("The numbers: ")
+    for number in numbers:
+        print(number)
