436effba384c061a337e82b3b42b220f59d9bbc2
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-11-03 11:25:41 -0600

    Completed exercises 34 and 35.

---
+++ Python_exercises/ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python3.8', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print("The animal at 1: ", animals[1])  # Python3.8
+print("The third animal: ", animals[2])  # Peacock
+print("The first animal: ", animals[0])  # bear
+print("The animal at 3: ", animals[3])  # kangaroo
+print("The fifth (5th) animal: ", animals[4])  # whale
+print("The animal at 2: ", animals[2])  # peacock
+print("The sixth (6th) animal: ", animals[5])  # platypus
+print("The animal at 4: ", animals[4])  # whale
